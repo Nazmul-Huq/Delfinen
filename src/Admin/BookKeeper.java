d197038237020890d2726831a6d0e8d6174e9bba
Søs <slys87@gmail.com>
2021-12-02 12:41:52 +0100

    Small update Bookkeeper

--- src/Admin/BookKeeper.java
+++ src/Admin/BookKeeper.java
@@ -2,29 +2,35 @@ package Admin;
 
 public class BookKeeper {
 
-    //Search for a person using their first name
+
+
+    //Search for a person using their name
     public void findMember() {
 
         //Search for an active member
-        ActiveMember.get
-        for (int i = 0; i <;i++) {
+        activeMember = Admin.get
+        for (int i = 0; i <; i++) {
 
         }
-
     }
 
+    //
+
+    //Calculate fees depending on membership types
     public void calculateFees() {
 
     }
 
+    //Calculate the total income of the club
     public void calculateIncome() {
 
     }
-
+    //Finding a persons debt
     public void calculateDebt() {
 
     }
 
+    //Trying to predict future income of the club
     public void calculateFutureIncome() {
 
     }
