6f64d83751da04b98001ce7c225dfb27ea59babd
Søs <slys87@gmail.com>
2021-12-03 11:40:14 +0100

    Files members

--- src/Admin/BookKeeper.java
+++ src/Admin/BookKeeper.java
@@ -1,30 +1,52 @@
 package Admin;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BookKeeper {
 
-    //Search for a person using their first name
+    //Search for a person using their name from Chairman
     public void findMember() {
 
-        //Search for an active member
-        //ActiveMember.get
-/*        for (int i = 0; i <;i++) {
+        ArrayList<String> membersList = new ArrayList<>();
+        //Find member from text file, get ArrayList
+        try {
+            Scanner scanner = new Scanner("Files/members.txt");
+            String gertNames;
 
-        }*/
+            while (scanner.hasNextLine()) {
+                String
+            }
+        }
 
-    }
 
+        //Search arraylist for id
+        //If we find the id, copy information for id
+        }
+
+
+
+    //
+
+    //Calculate fees depending on membership types
     public void calculateFees() {
 
+        //Find the member through id, taken from the method "findMember"
+
+
+
     }
 
+    //Calculate the total income of the club
     public void calculateIncome() {
 
     }
-
+    //Finding a persons debt
     public void calculateDebt() {
 
     }
 
+    //Trying to predict future income of the club
     public void calculateFutureIncome() {
 
     }
